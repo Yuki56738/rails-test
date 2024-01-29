c49c79add10e290e1bf2f40ca1e46e985316bd43
Yuki <github@risaton.net>
2024-01-29 22:57:15 +0900

    add show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,17 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def destroy
+    @articles = Article.find(params[:id])
+    @articles.destroy
+    redirect_to root_path, status: :see_other
+  end
+  def show
+    @articles = Article.find(params[:id])
+  end
   private
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
 end
