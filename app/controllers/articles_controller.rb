1f03448d109d785f891052a458e0cb471c2fe394
Yuki <github@risaton.net>
2024-01-29 21:52:49 +0900

    commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,25 @@
 class ArticlesController < ApplicationController
   def index
+    @articles =Article.all
+
+  end
+  def show
+    @articles = Article.find(params[:id])
+  end
+  def new
+    @articles = Article.new
+  end
+  def create
+    @articles = Article.new(article_params)
+
+    if @articles.save
+      redirect_to @articles
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
