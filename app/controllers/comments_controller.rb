0e2a8d823f5ddc27fac4b2bb5d739ea98f04e1f7
Yuki <github@risaton.net>
2024-01-29 23:09:05 +0900

    add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @articles = Article.find(params[:article_id])
+    @comment =
+      @articles.comments.create(comment_params)
+    redirect_to article_path(@articles)
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
