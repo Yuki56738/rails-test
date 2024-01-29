8c24f37e73ab63697b3d67b522093bfa89d5151f
Yuki <github@risaton.net>
2024-01-29 23:26:40 +0900

    commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,9 @@ class ArticlesController < ApplicationController
     @articles =Article.all
 
   end
-  def show
-    @articles = Article.find(params[:id])
-  end
+  # def show
+  #   @articles = Article.find(params[:id])
+  # end
   def new
     @articles = Article.new
   end
